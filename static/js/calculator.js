109b7376747ae839e33e135b0db0cec07dd5c1f9
Emilia <xiongyq7@mail2.sysu.edu.cn>
2017-10-01 22:23:36 +0800

    basic version

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,4 +1,4 @@
-var isNew = false;
+var isNew = false, isEnd = false;;
 function input(data, button){
     if(isNaN(data)){
         if(data == "C"){
@@ -8,16 +8,43 @@ function input(data, button){
         }
         if(data == "CE"){
             document.getElementById('number').innerHTML = "0";
+            if(isEnd){
+                document.getElementById('operations').innerHTML = "";
+                isEnd = false;
+            }
             return;
         }
         if(data == "Del"){
-            document.getElementById('number').innerHTML /= 10;
+            document.getElementById('number').innerHTML = Math.trunc(document.getElementById('number').innerHTML / 10);
+            if(isEnd){
+                document.getElementById('operations').innerHTML = "";
+                isEnd = false;
+            }
+            return;
+        }
+        if(data == "="){
+            if(isEnd)
+                return;
+            try{
+                document.getElementById('operations').innerHTML += document.getElementById('number').innerHTML;
+                document.getElementById('number').innerHTML = eval(document.getElementById('operations').innerHTML);
+                document.getElementById('operations').innerHTML += data;
+            }
+            catch(exception){
+                document.getElementById('number').innerHTML = "INVALID";
+            }
+            isNew = true;
+            isEnd = true;
             return;
         }
         document.getElementById('operations').innerHTML += document.getElementById('number').innerHTML + data;
         isNew = true;
     }
     else{
+        if(isEnd){
+            document.getElementById('operations').innerHTML = "";
+            isEnd = false;
+        }
         if(document.getElementById('number').innerHTML == "0" || isNew){
             document.getElementById('number').innerHTML = data;
             isNew = false;
