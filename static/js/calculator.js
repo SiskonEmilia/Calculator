6972c4492d043ccc5af1a8c39f3d2c8ca260f0b4
Emilia <xiongyq7@mail2.sysu.edu.cn>
2017-10-02 22:21:09 +0800

    sum

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -8,82 +8,83 @@ var clearOverflow = function(){
 };
 
 function input(data){
+    var idNum = document.getElementById('number'), idOpr = document.getElementById('operations');
     if(isNaN(data)){
         if(data == "C"){
-            document.getElementById('operations').innerHTML = "";
-            document.getElementById('number').innerHTML = "0";
+            idOpr.innerHTML = "";
+            idNum.innerHTML = "0";
             clearOverflow();
             return;
         }
         if(data == "CE"){
-            document.getElementById('number').innerHTML = "0";
+            idNum.innerHTML = "0";
             clearOverflow();
             if(isEnd){
-                document.getElementById('operations').innerHTML = "";
+                idOpr.innerHTML = "";
                 isEnd = false;
             }
             return;
         }
         if(data == "Del"){
-            document.getElementById('number').innerHTML = Math.trunc(document.getElementById('number').innerHTML / 10);
+            idNum.innerHTML = Math.trunc(idNum.innerHTML / 10);
             clearOverflow();
             if(isEnd){
-                document.getElementById('operations').innerHTML = "";
+                idOpr.innerHTML = "";
                 isEnd = false;
             }
             return;
         }
         if(data == "."){
             if(isEnd){
-                document.getElementById('operations').innerHTML = "";
+                idOpr.innerHTML = "";
                 isEnd = false;
             }
-            if(document.getElementById('number').innerHTML.includes(".")){
+            if(idNum.innerHTML.includes(".")){
                 return;
             }
-            document.getElementById('number').innerHTML += ".";
+            idNum.innerHTML += ".";
             return;
         }
         if(data == "±"){
             if(isEnd){
-                document.getElementById('operations').innerHTML = "";
+                idOpr.innerHTML = "";
                 isEnd = false;
             }
-            document.getElementById('number').innerHTML = -document.getElementById('number').innerHTML;
+            idNum.innerHTML = -idNum.innerHTML;
             return;
         }
         if(data == "="){
             if(isEnd)
                 return;
             try{
-                document.getElementById('operations').innerHTML += document.getElementById('number').innerHTML;
-                document.getElementById('number').innerHTML = eval(document.getElementById('operations').innerHTML);
-                document.getElementById('operations').innerHTML += data;
+                idOpr.innerHTML += idNum.innerHTML;
+                idNum.innerHTML = eval(idOpr.innerHTML);
+                idOpr.innerHTML += data;
                 clearOverflow();
             }
             catch(exception){
                 clearOverflow();
-                document.getElementById('number').innerHTML = "INVALID";
+                idNum.innerHTML = "INVALID";
             }
             isNew = true;
             isEnd = true;
             return;
         }
-        document.getElementById('operations').innerHTML += document.getElementById('number').innerHTML + data;
+        idOpr.innerHTML += idNum.innerHTML + data;
         isNew = true;
     }
     else{
         if(isEnd){
-            document.getElementById('operations').innerHTML = "";
+            idOpr.innerHTML = "";
             isEnd = false;
         }
-        if(document.getElementById('number').innerHTML == "0" || isNew){
-            document.getElementById('number').innerHTML = data;
+        if(idNum.innerHTML == "0" || isNew){
+            idNum.innerHTML = data;
             clearOverflow();
             isNew = false;
             return;
         }
-        document.getElementById('number').innerHTML += data;
+        idNum.innerHTML += data;
         clearOverflow();
     }
 };
